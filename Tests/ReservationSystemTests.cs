8215dbf3c6ffef811ddbdd40d81ba51ebc6d1560
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2022-07-24 14:11:30 +0200

    End of tutorial

--- Tests/ReservationSystemTests.cs
+++ Tests/ReservationSystemTests.cs
@@ -14,22 +14,57 @@ namespace Tests
     public class ReservationSystemTests
     {
         private readonly ReservationSystem reservationSystem;
+        private readonly Mock<IBookingService> bookingServiceMock = new();
 
         public ReservationSystemTests()
         {
+            //reservationSystem = new ReservationSystem(
+            //    new BookingService(new BookingDbContext()));
+
             reservationSystem = new ReservationSystem(
-                new BookingService(new BookingDbContext()));
+               bookingServiceMock.Object);
         }
 
         [Fact]
         public async Task ReserveAsync_Throws_When_No_Rooms_Available()
         {
-            
+            bookingServiceMock
+                .Setup(mock => mock.GetRoomsAsync(10))
+                .ReturnsAsync(Enumerable.Empty<Room>());
+
+            await Assert.ThrowsAsync<NoAvailableRoomsException>(
+                () => reservationSystem.ReserveAsync(10));
         }
 
         [Fact]
         public async Task ReserveAsync_Reserves_The_Available_Room()
         {
+            var reservedRoom = new Room
+            {
+                Beds = 10,
+                IsReserved = true,
+                RoomName = "Room 1"
+            };
+
+            var emptyRoom = new Room
+            {
+                Beds = 10,
+                IsReserved = false,
+                RoomName = "Room 2"
+            };
+
+            bookingServiceMock
+                .Setup(mock => mock.GetRoomsAsync(10))
+                .ReturnsAsync(new[] 
+                {
+                    reservedRoom,
+                    emptyRoom,
+                });
+
+            await reservationSystem.ReserveAsync(10);
+
+            bookingServiceMock
+                .Verify(mock => mock.ReserveRoomAsync(emptyRoom.RoomId), Times.Once());
         }
     }
 }
